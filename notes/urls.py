a517473c57ddb6b087826146d7d0ab2a707093bb
Zafarjanets <kodirovzafar35@gmail.com>
2026-06-19 16:39:11 +0300

    20min

--- notes/urls.py
+++ notes/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('contact/')
+    path('', views.contact, name='notes'),
+    path('create-notes', views.create_note, name='create_notes'),
+    path('update-notes/<int:pk>', views.update_note, name='update_notes'),
+    path('delete-notes/<int:pk>', views.delete_note, name='delete_notes')
 ]
--- notes/views.py
+++ notes/views.py
@@ -6,26 +6,65 @@ from .models import Contact
 def contact(request):
     contact=Contact.objects.filter(user=request.user)
     return render(request, 'contact.html',{'contact':contact})
-@login_required
-def create_note(request,pk):
-    if request.method=='POST':
-        name=request.POST.get('name')
-        phone=request.POST.get('phone')
-        img=request.FILET.get('img')
+
+def create_notes(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        img = request.FILES.get('img')
         
         if not name or not phone or not img:
-            return render(request,'create.html')
+            return render(request, 'create.html', {'error':'all fields necessary'})
         
-        note=Contact.objects.create(
+        note = Contact.objects.create(
             name=name,
             phone=phone,
-            img=img,
-            user=request.user
+            img=img
+            user = request.user
         )
-        return  redirect('notes')
+
+        return redirect('notes')
     
-    return render(request, 'create.html', {'title':'Create Notes'})
+    return render(request, 'create.html', {'note':'Create Notes'})
+
+
+@login_required
+def update_note(request, pk):
+    note = Contact.objects.get(pk=pk)
+    if request.user!=note.user:
+        return render(request, 'create.html', {'error':'You dont have permession'})
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        img = request.FILES.get('img')
+        if not name or not phone or not img:
+            return render(request, 'create.html', {'error':'all fields necessary'})
         
+        if request.user==note.user:
+            note.name = name
+            note.phone = phone
+            note.img=img
+            note.save()
+
+            return redirect('notes')
+    
+    return render(request, 'create.html', {'name':'Update Notes', 'note':note})
+
         
+
+@login_required
+def delete_note(request, pk):
+    
+    note = Contact.objects.get(pk=pk)
+    if request.user!=note.user:
+        return render(request, 'delete.html', {'error':'You dont have permession'})
+
+    if request.method=='POST':
+        if request.user == note.user:
+            note.delete()
+            return redirect('notes')
+    
+    return render(request, 'delete.html', {'note': note})
         
         
